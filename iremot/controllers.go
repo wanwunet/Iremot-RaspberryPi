6c9d37dbc62626d7011ee69ef18d8f38c0ab94b8
fenglei <fenglei@wanwunet.com>
2020-08-07 02:01:29 +0800

    屏蔽输出

--- iremot/controllers.go
+++ iremot/controllers.go
@@ -5,7 +5,6 @@ import (
 	"Iremot-RaspberryPi/device/rpio"
 	"Iremot-RaspberryPi/models"
 	"Iremot-RaspberryPi/util/json"
-	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -16,7 +15,7 @@ var setgpioSubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 	pin := rpio.Pin(device.Physical2BCM[mod.Pin])
 	pin.Output()
 	pin.Write(rpio.State(mod.State))
-	fmt.Println(mod)
+	//fmt.Println(mod)
 }
 
 //设置pin iremot/产品ID/设备ID/getgpio
@@ -25,7 +24,7 @@ var getgpioSubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 	json.Decode(msg.Payload(), &mod)
 	pin := rpio.Pin(device.Physical2BCM[mod.Pin])
 	pin.Input()
-	fmt.Println(mod)
+	//fmt.Println(mod)
 }
 
 //设置pin iremot/产品ID/设备ID/getgpio
@@ -36,5 +35,5 @@ var pwmgpioSubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 	pin.Mode(rpio.Pwm)
 	pin.Freq(mod.Freq)
 	pin.DutyCycle(mod.DutyLen, mod.CycleLen)
-	fmt.Println(mod)
+	//fmt.Println(mod)
 }
